3b1826c9c9a47791e42a12fb02fa9f92cfa43c08
gabrielrspupo <gabrielrspupo@gmail.com>
2018-03-20 21:37:17 -0300

    Menu principal

---
+++ bitwise-trie.asm
@@ -0,0 +1,41 @@
+	.data
+	.align 0
+menu:	.asciiz "===============================\nMENU\n\n1 - Inserir elemento\n2 - Remover elemento\n3 - Buscar elemento\n4 - Visualizar arvore\n5 - Sair do programa\n\n===============================\nOpção: "
+
+	.text
+	.globl main
+	
+main:	li $v0, 4		# imprimir menu na tela
+	la $a0, menu
+	syscall
+	
+	li $v0, 5		# ler opcao escolhida do teclado
+	syscall
+	move $t0, $v0
+	
+	li $t5, 1
+	li $t6, 2
+	li $t7, 3
+	li $t8, 4
+	li $t9, 5
+
+	# switch (1 = inserir, 2 = remover, 3 = buscar, 4 = ver arvore, 5 = sair)
+	beq $t0, $t5, insert
+	beq $t0, $t6, remove
+	beq $t0, $t7, search
+	beq $t0, $t8, visual
+	beq $t0, $t9, quit
+	
+insert:	j main
+	# TODO
+
+remove: j main
+	# TODO
+
+search: j main
+	# TODO
+
+visual: j main
+	# TODO
+
+quit:	# TODO
