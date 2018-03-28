6f332a86e96f6b6d44b816703845b8723fdd2233
gabrielrspupo <gabrielrspupo@gmail.com>
2018-03-28 20:46:25 -0300

    Alterações nos comentários

--- bitwise-trie.asm
+++ bitwise-trie.asm
@@ -1,20 +1,17 @@
 # =====================================================================================
-#		Trabalho 1 de Organização de Computadores Digitais I
+#		Trabalho 1 de Organizacao de Computadores Digitais I
 #
 #         Filename:  T1.asm 
-#  	  Description:  Implementação de uma Árvore de Busca Digital Binária 
+#  	  Description:  Implementacao de uma Arvore de Busca Digital Binaria 
 #
-#         Nome:	Eduardo Zaboto Mirolli			Nº USP:9778501
-#	  Participação:	
+#         Nome:	Eduardo Zaboto Mirolli			No USP:	9778501
+#	  Participacao:	
 #
-#         Nome:	Paulo Henrique Bodnarchuki da Cruz	Nº USP:	9790944
-#	  Participação:
+#         Nome:	Paulo Henrique Bodnarchuki da Cruz	No USP:	9790944
+#	  Participacao:
 #
-#         Nome:				 		Nº USP:
-#	  Participação:
-#
-#         Nome:						Nº USP:
-#	  Participação:
+#         Nome:	Gabriel Romualdo Silveira Pupo 		No USP: 9896250
+#	  Participacao:
 #
 # =====================================================================================
 
@@ -23,54 +20,61 @@
 	.align 0
 menu_str:	                .asciiz "===============================\nMENU\n\n1 - Inserir elemento\n2 - Remover elemento\n3 - Buscar elemento\n4 - Visualizar arvore\n5 - Sair do programa\n\n===============================\nOpção: "
 
-enter_insertion_str:		.asciiz "Digite o binário para inserção: "
+enter_insertion_str:		.asciiz "Digite o binario para insercao: "
 succeeded_insertion_str:	.asciiz "Chave inserida com sucesso. \n"
-repeated_insertion_str:		.asciiz "Chave repetida. Inserção não permitida \n"
-invalid_insertion_str:		.asciiz "Chave inválida. Insira somente números binários(ou -1 retorna ao menu) \n"
-enter_removal_str:		.asciiz "Digite o binário para remoção: "
+repeated_insertion_str:		.asciiz "Chave repetida. Insercao nao permitida \n"
+invalid_insertion_str:		.asciiz "Chave invalida. Insira somente numeros binarios(ou -1 retorna ao menu) \n"
+enter_removal_str:		.asciiz "Digite o binario para remocao: "
 succeeded_removal_str:		.asciiz "Chave removida com sucesso. \n"
-search_number_str:		.asciiz "Digite o binário para busca: "
+search_number_str:		.asciiz "Digite o binario para busca: "
 
-found_key_str:		.asciiz "Chave encontrada na arvore: "		
-not_found_key_str:	.asciiz "Chave não encontrada na arvore: "												
-path_str:		.asciiz "Caminho percorrido: "		
-menu_return_str:	.asciiz "Retornando ao menu. \n"
-
+found_key_str:			.asciiz "Chave encontrada na arvore: "		
+not_found_key_str:		.asciiz "Chave nao encontrada na arvore: "												
+path_str:			.asciiz "Caminho percorrido: "		
+menu_return_str:		.asciiz "Retornando ao menu. \n"
 
 	.text
 	.globl main
 	
-main:	li $v0, 4		# imprimir menu na tela
-	la $a0, menu_str
-	syscall
+mainloop:	li $v0, 4		# imprimir menu na tela
+		la $a0, menu_str
+		syscall
 	
-	li $v0, 5		# ler opcao escolhida do teclado
-	syscall
-	move $t0, $v0
+		li $v0, 5		# ler opcao escolhida do teclado
+		syscall
+		move $t0, $v0
 	
-	li $t5, 1
-	li $t6, 2
-	li $t7, 3
-	li $t8, 4
-	li $t9, 5
+		li $t5, 1
+		li $t6, 2
+		li $t7, 3
+		li $t8, 4
+		li $t9, 5
 
-	# switch (1 = inserir, 2 = remover, 3 = buscar, 4 = ver arvore, 5 = sair)
-	beq $t0, $t5, insert
-	beq $t0, $t6, remove
-	beq $t0, $t7, search
-	beq $t0, $t8, visual
-	beq $t0, $t9, quit
-	
-insert:	j main
-	# TODO
+		# switch (1 = inserir, 2 = remover, 3 = buscar, 4 = ver arvore, 5 = sair)
+		beq $t0, $t5, insert
+		beq $t0, $t6, remove
+		beq $t0, $t7, search
+		beq $t0, $t8, visual
+		beq $t0, $t9, quit
 
-remove: j main
-	# TODO
+		j main
+			
+#	struct node_trie {
+#		node *child_left;
+#		node *child_right;
+#		int terminator
+#	}
 
-search: j main
-	# TODO
+insert:		j main
+		# TODO
 
-visual: j main
-	# TODO
+remove: 	j main
+		# TODO
 
-quit:	# TODO
+search: 	j main
+		# TODO
+
+visual: 	j main
+		# TODO
+
+quit:		# TODO	
