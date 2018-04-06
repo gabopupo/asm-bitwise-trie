8460fd8eed5820248104161842fb218982ec408a
EdZM <eduardomirolli7@gmail.com>
2018-04-06 15:02:10 -0300

    Add files via upload

--- bitwise-trie.asm
+++ bitwise-trie.asm
@@ -34,6 +34,8 @@ not_found_key_str:		.asciiz "Chave nao encontrada na arvore: "
 path_str:			.asciiz "Caminho percorrido: "		
 menu_return_str:		.asciiz "Retornando ao menu. \n"
 
+success: .asciiz "Entrei na função \n"
+
 	.text
 	.globl main
 
@@ -43,7 +45,7 @@ main:
 	
 main_loop:
 	li $v0, 4		# imprimir menu na tela
-	la $a0, menu_str
+	la $a0, menu
 	syscall
 	
 	li $v0, 5		# ler opcao escolhida do teclado
@@ -71,39 +73,43 @@ read_str:
     li $a1, 16
     syscall
 
-    move $t1, $a0 # guardando o numero binario em um registrador para usÃ¡-lo durante esse processo
+    move $t1, $a0 # guardando o numero binario em um registrador para usá-lo durante esse processo
     
     lb $t0, 0($t1)
     beq $t0, 45, print_return # 45 == ascII para " - " ---> if(string[i] == '-') 
 
-str_checker_loop:
+str_checker_loop:    
    
-    beq $t0, 10 , TenToZero	#TenToZero Ã© a conversÃ£o do 10 == ENTER para um \0 
+    beq $t0, 10 , TenToZero	#TenToZero é a conversão do 10 == ENTER para um \0 
     beq $t0, $zero, end_loop	# if( string[i] == '\0' )  
     bgt $t0, 49, print_err	# if( string[i] > 49 ) --> se o caractere for maior do que o 1 em ASCII, imprima uma mensagem de erro 
     blt $t0, 48, print_err	# if( string[i] < 48 )
     
-    addi $t1, $t1, 1		# some 1 ao endereÃ§o base da string para ter acesso ao seu proximo caractere
+    
+    addi $t1, $t1, 1		# some 1 ao endereço base da string para ter acesso ao seu proximo caractere
     lb $t0, ($t1)		# carregue o proximo byte da string no byte mais a direita do registrador $t0
     j str_checker_loop
 
+
 TenToZero:
-	li $t0, 0
 	
+	li $t0, 0
+	sb $t0, ($t1)
 	
 end_loop:
-	move $s0, $a0 # no caso de o numero digitado ser vÃ¡lido, ele serÃ¡ salvo em um registrador s0
+	move $s0, $a0 # no caso de o numero digitado ser válido, ele será salvo em um registrador s0
+			#problema: o a0 armazenado 
 	jr $ra
 
 
 print_err:
-    li $v0, 4
-    la $a0, invalid_insertion_str
-    syscall
+   	li $v0, 4	
+   	la $a0, invalid_insertion_str
+  	syscall
     
-    li $s0, -1 	#carregando um valor negativo em s0 para marcar que a leitura da entrada encontrou um erro
+    	li $s0, -1 	#carregando um valor negativo em s0 para marcar que a leitura da entrada encontrou um erro
 	
-    jr $ra
+    	jr $ra
     
     
 
@@ -112,19 +118,53 @@ print_return:
     la $a0, menu_return_str
     syscall
 
-    j main_loop
+    j main_loop	
+
+
+create_node:	
+		
+		
+		
+		subi $sp, $sp, 4		# armazenar o endereco de retorno na pilha
+		sw $ra, 0($sp)
+		
+		li $v0, 9			# alocar 12 (4*3) bytes na memoria
+		li $a0, 12
+		syscall
+		
+		li $t0, 1
+		sw $zero, 0($v0)		# node->child_left = NULL;
+		sw $zero, 4($v0)		# node->child_right = NULL;
+		sw $t0, 8($v0)			# node->terminator = TRUE;
+		
+		lw $ra, 0($sp)
+		addi $sp, $sp, 4
+	
+		jr $ra		
+
+
 
 insert:		
+	
    	li $v0, 4 
    	la $a0, enter_insertion_str
 	syscall
 
 	jal read_str
+	bgezal $s0, insert_loop #se o valor contido em s0 for maior ou igual a zero , pode-se criar um nó valido
+		
 	
-	move $a0, $s1			# guarde temporariamente a raiz da arvore
+		
+	j insert
+
+		# TODO
 
 insert_loop:	
-	beq $s0, $zero, end_insert_loop # condiÃ§Ã£o de parada!
+	
+	lb $t3, ($s0)
+	
+	beq $t3, $zero, end_insert_loop # condição de parada!
+	
 	
 	li $t3 , 48			# 48 == 0 em ascII
 	beq $s0, $t3, insert_left	# se num[i] == 0, inserir a esquerda da raiz
@@ -140,6 +180,13 @@ insert_loop:
 
 	j insert_loop
 
+	
+#	struct node_trie {
+#		node_trie *child_left;
+#		node_trie *child_right;
+#		int terminator
+#	}
+
 insert_left:
 	lw $t2, 0($s1)			# carregue o conteudo de node_left
 	seq $t0, $t2, $zero		# se node_left == NULL, t0 = 1, do contrario, t0 = 0
@@ -151,36 +198,16 @@ insert_left:
 	addi $s0, $s0, 1		# acessar o proximo indice do numero (isto eh, i++)
 	j insert_loop			# volte ao loop de insercao
 	
-	# TODO
-
 end_insert_loop:
 	li $t4, 1 			# O 1 representa um true booleano
-	sw $t4, 8($s1) 			# O true serÃ¡ atribuÃ­do ao terminador no ultimo nÃ³ que serÃ¡ inserido
+	sw $t4, 8($s1) 			# O true será atribuído ao terminador no ultimo nó que será inserido
 	j insert
 
-	
-#	struct node_trie {
-#		node_trie *child_left;
-#		node_trie *child_right;
-#		int terminator
-#	}
 
-create_node:	
-		subi $sp, $sp, 4		# armazenar o endereco de retorno na pilha
-		sw $ra, 0($sp)
-		
-		li $v0, 9			# alocar 12 (4*3) bytes na memoria
-		li $a0, 12
-		syscall
-		
-		li $t0, 1
-		sw $zero, 0($v0)		# node->child_left = NULL;
-		sw $zero, 4($v0)		# node->child_right = NULL;
-		sw $t0, 8($v0)		# node->terminator = TRUE;
-		
-		lw $ra, 0($sp)
-		addi $sp, $sp, 4
-		jr $ra		
+
+
+
+
 
 remove: 
     li $v0, 4 
